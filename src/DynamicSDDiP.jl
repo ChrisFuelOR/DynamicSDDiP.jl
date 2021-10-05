6a020dab2802bbe4795550549fd03e5bb39ad581
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-10-05 18:43:24 -0400

    Basic changes to module-specific files

--- src/DynamicSDDiP.jl
+++ src/DynamicSDDiP.jl
@@ -1,5 +1,45 @@
+# Copyright (c) 2021 Christian Fuellner <christian.fuellner@kit.edu>
+
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+################################################################################
+
 module DynamicSDDiP
 
+#import JuMP
+import SDDP
+import Delaunay
+import Gurobi
+import PiecewiseLinearOpt
+import Revise
+import TimerOutputs
+import GAMS
+#import SCIP
+import MathOptInterface
+import Distances
+import LinearAlgebra
+using Printf
+using Dates
+
+import Reexport
+Reexport.@reexport using JuMP
+
+using Infiltrator
+
 # Write your package code here.
+include("state.jl")
+include("JuMP.jl")
+include("typedefs.jl")
+include("logging.jl")
+include("stopping.jl")
+include("objective.jl")
+include("bellman.jl")
+
+include("solveStarter.jl")
+include("algorithm.jl")
+include("piecewiseLinearRelaxation.jl")
+include("simplexOperations.jl")
+include("problemModifications.jl")
+include("lagrange.jl")
 
 end
