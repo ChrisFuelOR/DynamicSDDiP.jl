660dd7c577849d6c8ac4bc175fe55198d52f98b2
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-10-05 11:15:28 -0400

    Changes to typedefs (excluding outer loop)

--- src/typedefs.jl
+++ src/typedefs.jl
@@ -1,6 +1,5 @@
 # The structs
 # > "NonlinearCut",
-# > "OuterLoopIterationResult"
 # > "InnerLoopIterationResult"
 # > "BackwardPassItems"
 # are derived from similar named structs (Cut, IterationResult, BackwardPassItems)
@@ -22,10 +21,8 @@ import Revise
 
 # Struct for algorithmic parameters
 # ------------------------------------------------------------------------------------------------------------------
-# epsilon_outerLoop: optimality tolerance for outer loop
 # epsilon_innerLoop: optimality tolerance for inner loop
 # binaryPrecision: epsilons for latest/current binary expansion (better use vector instead of dict?)
-# plaPrecision: precision of the initial PLA (same for all stages, but can differ between nonlinearities so far)
 # sigma: parameters used to obtain the regularized problem (better vector?)
 # ------------------------------------------------------------------------------------------------------------------
 # possible extensions:
@@ -37,9 +34,7 @@ import Revise
 # Mutable struct for algorithmic parameters that may change during the iterations
 # Vector{Float64} or Dict{Int64, Float64}?
 mutable struct AlgoParams
-    epsilon_outerLoop :: Float64 # optimality tolerance for outer loop (relative!)
     epsilon_innerLoop :: Float64 # optimality tolerance for inner loop (relative!)
-    #binaryPrecision :: Vector{Float64} # Epsilons for latest/current binary expansion (better vector?)
     binaryPrecision :: Dict{Symbol, Float64}
     sigma :: Vector{Float64} # parameters used to obtain the regularized problem (better vector?)
     sigma_factor :: Float64
@@ -54,12 +49,10 @@ mutable struct AlgoParams
     level_factor :: Float64
     cut_selection :: Bool
     lag_status_regime :: Symbol
-    outer_loop_strategy :: Symbol
 end
 
 # Struct for initial algorithmic parameters that remain fixed and characterize a model run
 struct InitialAlgoParams
-    epsilon_outerLoop :: Float64
     epsilon_innerLoop :: Float64
     binaryPrecision :: Dict{Symbol, Float64}
     plaPrecision :: Array{Vector{Float64},1}
@@ -75,69 +68,12 @@ struct InitialAlgoParams
     level_factor :: Float64
     cut_selection :: Bool
     lag_status_regime :: Symbol
-    outer_loop_strategy :: Symbol
-end
-
-# struct for Simplex
-mutable struct Simplex
-    vertices :: Array{Float64,2}
-    vertice_values :: Vector{Float64}
-    maxOverestimation :: Float64
-    maxUnderestimation :: Float64
-end
-
-# struct for Triangulation
-# better to use dicts instead of vectors without index?
-mutable struct Triangulation
-    simplices :: Vector{Simplex}
-    plaPrecision_vector :: Vector{Float64}
-    plrVariables :: Vector{JuMP.VariableRef}
-    plrConstraints :: Vector{JuMP.ConstraintRef}
-    # An extension dictionary.
-    ext::Dict{Symbol,Any}
-end
-
-# struct for nonlinear functions
-# Specify first argument to type of user-defined function?
-mutable struct NonlinearFunction
-    nonlinfunc_eval :: Any # for evaluation
-    nonlinfunc_exp :: Any # for constraint building
-    auxVariable :: JuMP.VariableRef # for definition of (PLA) constraints
-    # refToNonlinearConstraint :: JuMP.ConstraintRef # just for allocation # not used anymore and does not work for add_NL_constraint
-    variablesContained :: Vector{JuMP.VariableRef} # for getting bounds for Triangulation
-    triangulation :: Union{Triangulation, Nothing} # to store related Triangulation
-    shift :: Symbol #either :shift or :noshift (for some concave cases)
-    refineType :: Symbol #either :replace or :keep
-    ext::Dict{Symbol,Any} # required to store solutions later
-
-    function NonlinearFunction(
-        nonlinfunc_eval::Any, # Function
-        nonlinfunc_exp::Any, #Function
-        auxVariable::JuMP.VariableRef,
-        #refToNonlinearConstraint::JuMP.ConstraintRef,
-        variablesContained::Vector{JuMP.VariableRef},
-        shift::Symbol,
-        refineType::Symbol,
-         )
-        return new(
-            nonlinfunc_eval,
-            nonlinfunc_exp,
-            auxVariable,
-            #refToNonlinearConstraint,
-            variablesContained,
-            nothing,
-            shift,
-            refineType,
-            Dict{Symbol,Any}()
-        )
-    end
 end
 
 # struct for solvers to be used (maybe mutable)
 struct AppliedSolvers
     LP :: Any
     MILP :: Any
-    MINLP :: Any
     NLP :: Any
     Lagrange :: Any
 end
@@ -159,25 +95,12 @@ mutable struct NonlinearCut
     non_dominated_count::Int # SDDP
     iteration::Int64
 end
-    # TODO: Do we need to store the trial point also in binary form?
-    # I think not because we can always determine it using trial_state
-    # and binary_precision.
-    # TODO: If the binary precision may be different for all components,
-    # then we have to adapt this.
-    # TODO: Should we also store the expression of this cut in binary space?
-    # TODO: Do we want to store the iteration as well?
-
-# struct for outer loop iteration results
-struct OuterLoopIterationResult#{T}
-    # pid
-    lower_bound :: Float64 # here the inner or outer loop lower bound can be used
-    upper_bound :: Float64 # should be renamed as cumulative_value as in SDDP if we solve stochastic problems
-    current_sol :: Array{Dict{Symbol,Float64},1} #Vector{Dict{Symbol, Float64}}
-    has_converged :: Bool
-    status :: Symbol # solution status (i.e. number of iterations)
-    #nonlinearCuts :: Dict{T, Vector{Any}} # only required for logging, binary explanation
-    # however, then also binary precision / K should be stored for interpretability
-end
+    # TODO:
+    # 1) Do we need to store the trial point also in binary form (even though
+    # we can always determine it using trial_state and binary_precision)?
+    # 2) So far, binary precision is the same for all components.
+    # 3) Should we also store the expression of this cut in binary space?
+    # 4) Do we want to store the iteration as well?
 
 # struct for inner loop iteration results
 mutable struct InnerLoopIterationResult{T,S}
