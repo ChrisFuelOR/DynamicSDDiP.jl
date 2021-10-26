98837bc5bb8b1950c7260ce51b12256e8e1b7eeb
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-10-26 17:06:55 -0400

    Tests for LevelBundle

--- examples/UC_10_10.jl
+++ examples/UC_10_10.jl
@@ -46,10 +46,10 @@ function model_config()
 
     # Duality / Cut computation configuration
     dual_initialization_regime = DynamicSDDiP.ZeroDuals()
-    dual_solution_regime = DynamicSDDiP.Kelley()
+    dual_solution_regime = DynamicSDDiP.LevelBundle()
     dual_bound_regime = DynamicSDDiP.ValueBound()
     dual_status_regime = DynamicSDDiP.Lax()
-    dual_choice_regime = DynamicSDDiP.MagnantiWongChoice()
+    dual_choice_regime = DynamicSDDiP.StandardChoice()
     duality_regime = DynamicSDDiP.LagrangianDuality(
         atol = 1e-4,
         rtol = 1e-4,
