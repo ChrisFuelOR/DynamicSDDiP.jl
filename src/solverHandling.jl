c940f1e430789d5943b92b439c3ae2b6ec0b7e11
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-10-27 16:51:58 -0400

    Corrected a typo

--- src/solverHandling.jl
+++ src/solverHandling.jl
@@ -23,7 +23,7 @@ function set_solver!(
         if isa(cut_projection_regime, DynamicSDDiP.KKT)
             solver = applied_solvers.MINLP
         elseif isa(cut_projection_regime, DynamicSDDiP.StrongDuality)
-            solver = applide_solvers.MINLP
+            solver = applied_solvers.MINLP
             #solver = applied_solvers.MIQCP
         else
             solver = applied_solvers.MILP
