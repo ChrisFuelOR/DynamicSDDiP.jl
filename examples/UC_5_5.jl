4ac032212e7b4e97a619b0d28b1ee351cd8afe24
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2021-10-26 21:42:59 -0400

    Prepared a new run

--- examples/UC_5_5.jl
+++ examples/UC_5_5.jl
@@ -46,10 +46,10 @@ function model_config()
 
     # Duality / Cut computation configuration
     dual_initialization_regime = DynamicSDDiP.ZeroDuals()
-    dual_solution_regime = DynamicSDDiP.LevelBundle()
-    dual_bound_regime = DynamicSDDiP.ValueBound()
+    dual_solution_regime = DynamicSDDiP.Kelley()
+    dual_bound_regime = DynamicSDDiP.BothBounds()
     dual_status_regime = DynamicSDDiP.Lax()
-    dual_choice_regime = DynamicSDDiP.StandardChoice()
+    dual_choice_regime = DynamicSDDiP.MangnantiWongChoice()
     duality_regime = DynamicSDDiP.LagrangianDuality(
         atol = 1e-4,
         rtol = 1e-4,
