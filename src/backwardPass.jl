51a503ec4284a6a40db982a317b5ad3095e07e20
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2022-07-13 23:09:23 +0200

    Removed some infiltrate

--- src/backwardPass.jl
+++ src/backwardPass.jl
@@ -277,8 +277,6 @@ function solve_first_stage_problem(
     stage_objective = JuMP.value(node.stage_objective)
     objective = JuMP.objective_value(subproblem)
 
-    Infiltrator.@infiltrate
-
     ############################################################################
     # DETERMINE THE PROBLEM SIZE
     ############################################################################
