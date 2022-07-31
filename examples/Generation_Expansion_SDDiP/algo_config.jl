8af075294316e6eac69ffcdc22bad72ec6f939c5
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2022-08-01 00:24:15 +0200

    Try runs with numerical focus

--- examples/Generation_Expansion_SDDiP/algo_config.jl
+++ examples/Generation_Expansion_SDDiP/algo_config.jl
@@ -23,7 +23,7 @@ function algo_config(
     dual_solution_regime = DynamicSDDiP.Kelley()
     dual_bound_regime = DynamicSDDiP.BothBounds()
     dual_status_regime = DynamicSDDiP.Lax()
-    dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
+    dual_choice_regime = DynamicSDDiP.StandardChoice()
     #dual_subproblemace_regime = DynamicSDDiP.BendersSpanSpaceRestriction(10, :multi_cut)
     dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
@@ -41,7 +41,7 @@ function algo_config(
             normalization_regime = normalization_regime,
             dual_space_regime = dual_space_regime,
             copy_regime = copy_regime,
-            user_dual_multiplier_bound = 10.0,
+            user_dual_multiplier_bound = 10.0, # 10.0
             #user_dual_objective_bound = 1e4,
         )
     elseif duality_regime_sym == :lag
@@ -114,7 +114,7 @@ function algo_config(
         silent = silent,
         infiltrate_state = infiltrate_state,
         solver_approach = solver_approach,
-        numerical_focus = false,
+        numerical_focus = true,
         run_numerical_stability_report = false,
         seed = forward_seed,
         run_description = ""
