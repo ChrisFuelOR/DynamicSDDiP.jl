08ed175ff440851642221cadb29ac9983eb83b96
Christian Fuellner <christian.fuellner@kit.edu>
2022-08-24 15:06:06 +0200

    Test of combination of SB and Lagrange

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -22,11 +22,11 @@ function algo_config(
 
     # Duality / Cut computation configuration
     dual_initialization_regime = DynamicSDDiP.ZeroDuals()
-    dual_solution_regime = DynamicSDDiP.LevelBundle()
+    dual_solution_regime = DynamicSDDiP.Kelley()
     dual_bound_regime = DynamicSDDiP.BothBounds()
     dual_status_regime = DynamicSDDiP.Lax()
 
-    dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
+    dual_choice_regime = DynamicSDDiP.StandardChoice()
     if isa(normalization_regime, DynamicSDDiP.L∞_Deep)
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
@@ -73,11 +73,11 @@ function algo_config(
     state_approximation_regime = DynamicSDDiP.NoStateApproximation()
 
     # Cut generation regimes
-    cut_generation_regime = DynamicSDDiP.CutGenerationRegime(
+    cut_generation_regime_2 = DynamicSDDiP.CutGenerationRegime(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
-        #cut_away_approach = true,
-        #iteration_to_start = 51,
+        cut_away_approach = true,
+        iteration_to_start = 31,
     )
 
     cut_generation_regime_1 = DynamicSDDiP.CutGenerationRegime(
@@ -85,7 +85,7 @@ function algo_config(
         duality_regime = DynamicSDDiP.StrengthenedDuality(),
     )
 
-    cut_generation_regimes = [cut_generation_regime]
+    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
