925543d5343df444dc8844ad77c52bb848d718d3
Christian Fuellner <christian.fuellner@kit.edu>
2022-09-09 10:42:54 +0200

    Test CLSP (10,20) with Bundle method and combination of SB and Lag cuts, but without cut_away approach

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -76,7 +76,7 @@ function algo_config(
     cut_generation_regime_2 = DynamicSDDiP.CutGenerationRegime(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
-        cut_away_approach = true,
+        cut_away_approach = false,
         iteration_to_start = 21,
     )
 
