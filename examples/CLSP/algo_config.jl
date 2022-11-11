1074fdb917e3f0c56a435e52ecf3a15aece34153
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2022-11-11 10:59:14 +0100

    New test bd 0.01

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -48,8 +48,8 @@ function algo_config(
             normalization_regime = normalization_regime,
             dual_space_regime = dual_space_regime,
             copy_regime = copy_regime,
-            user_dual_multiplier_bound = 20.0, # 10.0
-            #user_dual_objective_bound = 1e4,
+            user_dual_multiplier_bound = 0.01, # 10.0
+            #user_dual_objective_bound = 0.01,
         )
     elseif duality_regime_sym == :lag
         duality_regime = DynamicSDDiP.LagrangianDuality(
