f0070c94963e684b86999fcad56f82725c5a3a8f
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2022-11-14 13:49:29 +0100

    Another correction for ws03 run

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -31,8 +31,8 @@ function algo_config(
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
 
-    dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
-    #dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
+    #dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
+    dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
 
     if duality_regime_sym == :uni_lag
@@ -77,7 +77,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        iteration_to_start = 21,
+        #iteration_to_start = 21,
         #iteration_to_stop = 21,
     )
 
@@ -86,7 +86,7 @@ function algo_config(
         duality_regime = DynamicSDDiP.LinearDuality(),
     )
 
-    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
