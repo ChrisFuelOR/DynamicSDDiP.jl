e0c11b3b3c4910738e4aab51c2886ecf6b5eada0
Christian Fuellner <christian.fuellner@kit.edu>
2022-11-26 18:12:58 +0100

    New test on ws03

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -48,8 +48,8 @@ function algo_config(
             normalization_regime = normalization_regime,
             dual_space_regime = dual_space_regime,
             copy_regime = copy_regime,
-            user_dual_multiplier_bound = 10.0, # 10.0
-            #user_dual_objective_bound = 1e4,
+            #user_dual_multiplier_bound = 10.0, # 10.0
+            user_dual_objective_bound = 1e4,
         )
     elseif duality_regime_sym == :lag
         duality_regime = DynamicSDDiP.LagrangianDuality(
