3663f3763f13a432d382fda4bc3c1eed7a298991
Christian Fuellner <christian.fuellner@kit.edu>
2022-12-03 08:17:08 +0100

    Start CL on ws02 for (4,20)

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -31,7 +31,7 @@ function algo_config(
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
 
-    #dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
+    dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
     dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
 
