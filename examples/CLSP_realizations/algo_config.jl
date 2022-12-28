c3e2d789a9ecdf0e687eab5ccdbbae088d45f0f1
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2022-12-28 21:07:27 +0100

    Re-start (16,50) examples

--- examples/CLSP_realizations/algo_config.jl
+++ examples/CLSP_realizations/algo_config.jl
@@ -31,8 +31,8 @@ function algo_config(
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
 
-    #dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
-    dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
+    dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
+    #dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
 
     if duality_regime_sym == :uni_lag
@@ -149,8 +149,8 @@ function algo_config(
         cut_selection_regime = cut_selection_regime,
         cut_generation_regimes = cut_generation_regimes,
         simulation_regime = simulation_regime,
-        #late_binarization_regime = DynamicSDDiP.NoLateBinarization(),
-        late_binarization_regime = DynamicSDDiP.LateBinarization(K, 41),
+        late_binarization_regime = DynamicSDDiP.NoLateBinarization(),
+        #late_binarization_regime = DynamicSDDiP.LateBinarization(K, 41),
         cut_type = cut_type,
         log_file = log_file,
         silent = silent,
