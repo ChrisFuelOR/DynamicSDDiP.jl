ae0bc269c4d2689018b1adebbc08e37ba48b97db
Christian Fuellner <christian.fuellner@kit.edu>
2023-01-31 17:38:51 +0100

    TEST RUNS CLSP(16,20,3) Part 26

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -31,7 +31,7 @@ function algo_config(
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
 
-    dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(10, :multi_cut)
+    dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
     #dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
 
@@ -143,7 +143,7 @@ function algo_config(
         cut_generation_regimes = cut_generation_regimes,
         simulation_regime = simulation_regime,
         #late_binarization_regime = DynamicSDDiP.NoLateBinarization(),
-        late_binarization_regime = DynamicSDDiP.LateBinarization(K, 21),
+        late_binarization_regime = DynamicSDDiP.LateBinarization(K, 11),
         cut_type = cut_type,
         log_file = log_file,
         silent = silent,
