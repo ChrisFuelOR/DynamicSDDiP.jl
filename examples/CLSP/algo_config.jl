c5f150f99a777603ef4abb71f8713eb0607c1ea9
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2023-03-26 12:25:59 +0200

    New runs for CL w/o bin

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -32,8 +32,8 @@ function algo_config(
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
 
-    #dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(10, :multi_cut)
-    dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
+    dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
+    #dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
 
     if duality_regime_sym == :uni_lag
@@ -94,8 +94,8 @@ function algo_config(
     cut_generation_regime_2 = DynamicSDDiP.CutGenerationRegime(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
-        cut_away_approach = false,
-        #iteration_to_start = 21,
+        #cut_away_approach = false,
+        iteration_to_start = 21,
         #iteration_to_stop = 30,
     )
 
@@ -105,7 +105,7 @@ function algo_config(
         #iteration_to_start = 1,
     )
 
-    cut_generation_regimes = [cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
