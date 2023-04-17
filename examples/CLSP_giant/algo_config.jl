88cd5673792f8d05df6d52e338cbfc5e5d51f954
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2023-04-17 11:31:10 +0200

    Giant CLSP with SB

--- examples/CLSP_giant/algo_config.jl
+++ examples/CLSP_giant/algo_config.jl
@@ -26,10 +26,10 @@ function algo_config(
     dual_bound_regime = DynamicSDDiP.BothBounds()
     dual_status_regime = DynamicSDDiP.Lax()
 
-    dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
-    if isa(normalization_regime, DynamicSDDiP.L∞_Deep)
-        dual_choice_regime = DynamicSDDiP.StandardChoice()
-    end
+    dual_choice_regime = DynamicSDDiP.StandardChoice()
+    #if isa(normalization_regime, DynamicSDDiP.L∞_Deep)
+    #    dual_choice_regime = DynamicSDDiP.StandardChoice()
+    #end
 
     #dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(10, :multi_cut)
     dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
@@ -94,7 +94,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        #iteration_to_start = 21,
+        iteration_to_start = 21,
         #iteration_to_stop = 21,
     )
 
@@ -103,7 +103,7 @@ function algo_config(
         duality_regime = DynamicSDDiP.StrengthenedDuality(),
     )
 
-    cut_generation_regimes = [cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
