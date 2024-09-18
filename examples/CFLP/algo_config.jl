db712d88e9401c4901bdc7042fad411d2cbeeca8
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2024-09-18 14:38:24 +0200

    Test with sb_opt on server

--- examples/CFLP/algo_config.jl
+++ examples/CFLP/algo_config.jl
@@ -16,9 +16,9 @@ function algo_config(
     )
 
     # Stopping rules to be used
+    stopping_rules = [SDDP.TimeLimit(time_limit), SDDP.BoundStalling(20,1e-4)]
     #stopping_rules = [SDDP.TimeLimit(time_limit), SDDP.BoundStalling(20,1e-4)]
-    #stopping_rules = [SDDP.TimeLimit(time_limit), SDDP.BoundStalling(20,1e-4)]
-    stopping_rules = [SDDP.IterationLimit(20)]
+    #stopping_rules = [SDDP.IterationLimit(20)]
     #stopping_rules = [ SDDP.IterationLimit(20), SDDP.BoundStalling(20,1e-4)]
 
     # Duality / Cut computation configuration
@@ -40,7 +40,7 @@ function algo_config(
         duality_regime = DynamicSDDiP.UnifiedLagrangianDuality(
             atol = 1e-2,
             rtol = 1e-2,
-            iteration_limit = 1000,
+            iteration_limit = 100,
             dual_initialization_regime = dual_initialization_regime,
             dual_bound_regime = dual_bound_regime,
             dual_solution_regime = dual_solution_regime,
@@ -54,7 +54,7 @@ function algo_config(
         duality_regime = DynamicSDDiP.LagrangianDuality(
             atol = 1e-2,
             rtol = 1e-2,
-            iteration_limit = 1000,
+            iteration_limit = 100,
             dual_initialization_regime = dual_initialization_regime,
             dual_bound_regime = dual_bound_regime,
             dual_solution_regime = dual_solution_regime,
@@ -76,7 +76,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        iteration_to_start = 11,
+        iteration_to_start = 1,
         #iteration_to_stop = 30,
     )
 
@@ -87,7 +87,7 @@ function algo_config(
          #cut_away_approach = false,
     )
 
-    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
