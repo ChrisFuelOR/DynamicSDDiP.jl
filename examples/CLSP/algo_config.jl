611f27c0fff764ffa640835c9bab8419f64b3faf
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2024-10-10 15:12:35 +0200

    Corrected previous commit

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -78,7 +78,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        #iteration_to_start = 21,
+        iteration_to_start = 1,
         #iteration_to_stop = 30,
     )
 
@@ -128,8 +128,8 @@ function algo_config(
         cut_selection_regime = cut_selection_regime,
         cut_generation_regimes = cut_generation_regimes,
         simulation_regime = simulation_regime,
-        #late_binarization_regime = DynamicSDDiP.NoLateBinarization(),
-        late_binarization_regime = DynamicSDDiP.LateBinarization(K, 41),
+        late_binarization_regime = DynamicSDDiP.NoLateBinarization(),
+        #late_binarization_regime = DynamicSDDiP.LateBinarization(K, 41),
         cut_type = cut_type,
         log_file = log_file,
         silent = silent,
