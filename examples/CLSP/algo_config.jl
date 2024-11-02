c076d24762496393a12ed7f1662bf52a1330142a
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2024-11-02 17:15:35 +0100

    Further tests for Lsup

--- examples/CLSP/algo_config.jl
+++ examples/CLSP/algo_config.jl
@@ -76,7 +76,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        #iteration_to_start = 21,
+        iteration_to_start = 21,
         #iteration_to_stop = 30,
     )
 
@@ -87,7 +87,7 @@ function algo_config(
     #     #cut_away_approach = false,
     )
 
-    cut_generation_regimes = [cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
