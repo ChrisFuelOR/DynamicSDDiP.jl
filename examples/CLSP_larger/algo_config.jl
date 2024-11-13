c2356ad9126bf6a88204d6c7f11d49ca29658a14
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2024-11-13 09:17:34 +0100

    Re-run ws02 SB tests on ws03

--- examples/CLSP_larger/algo_config.jl
+++ examples/CLSP_larger/algo_config.jl
@@ -31,7 +31,7 @@ function algo_config(
         dual_choice_regime = DynamicSDDiP.MinimalNormChoice()
     end
 
-    ual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
+    # dual_space_regime = DynamicSDDiP.BendersSpanSpaceRestriction(20, :multi_cut)
     dual_space_regime = DynamicSDDiP.NoDualSpaceRestriction()
     copy_regime = DynamicSDDiP.ConvexHullCopy()
 
@@ -75,7 +75,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        #iteration_to_start = 21,
+        iteration_to_start = 21,
         #iteration_to_stop = 21,
     )
 
@@ -84,7 +84,7 @@ function algo_config(
         duality_regime = DynamicSDDiP.StrengthenedDuality(),
     )
 
-    cut_generation_regimes = [cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_1, cut_generation_regime_2]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
