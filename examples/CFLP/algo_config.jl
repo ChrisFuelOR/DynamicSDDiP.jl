804a58c691ddff4e80f3320e62651ee71ebeb8ed
ChrisFuelOR <72441442+ChrisFuelOR@users.noreply.github.com>
2025-09-20 19:41:54 +0200

    Test using SB cuts

--- examples/CFLP/algo_config.jl
+++ examples/CFLP/algo_config.jl
@@ -75,7 +75,7 @@ function algo_config(
         state_approximation_regime = state_approximation_regime,
         duality_regime = duality_regime,
         #cut_away_approach = false,
-        iteration_to_start = 1,
+        iteration_to_start = 21,
         #iteration_to_stop = 30,
     )
 
@@ -86,7 +86,7 @@ function algo_config(
          #cut_away_approach = false,
     )
 
-    cut_generation_regimes = [cut_generation_regime_2]
+    cut_generation_regimes = [cut_generation_regime_2, cut_generation_regime_1]
 
     # Regularization configuration
     regularization_regime = DynamicSDDiP.NoRegularization()
